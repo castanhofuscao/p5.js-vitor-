22fc0659b008b88a1d6d6511ef96b7b76b7eda51
castanhofuscao <00001107110713sp@al.educacao.sp.gov.br>
2024-05-24 08:19:03 -0300

    Add files via upload

---
+++ sketch (1).js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+  background("indigo")
+}
+function draw() {
+
+  stroke("white");
+  fill("purple");
+  
+  if(mouseIsPressed){
+  circle(mouseX,mouseY, 200, 200);
+  }
+}
